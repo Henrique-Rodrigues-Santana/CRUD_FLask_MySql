6946bec570b62f8bf4590f7376c127383625d7e2
Henrique Rodrigues Santana <henrique.rocdrigue@gmail.com>
2024-09-04 23:02:28 -0300

    Inserindo Dados
    Enviando dados para o banco de dados.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -14,12 +14,14 @@ Port = 3306
 User = 'root'
 Pass = ''
 Database = 'db-crud'
+Cursor_Class = 'DictCursor'
 
 app.config['MYSQL_HOST'] = Host
 app.config['MYSQL_PORT'] = Port
 app.config['MYSQL_USER'] = User
 app.config['MYSQL_PASSWORD'] = Pass
 app.config['MYSQL_DB'] = Database
+app.config['MYSQL_CURSORCLASS'] = Cursor_Class
 
 mysql.init_app(app)
 
@@ -43,6 +45,26 @@ def index_clientes():
 
     return render_template('modulos/clientes/index.html', clientes_t = clientes)
 
+
+@app.route('/clientes/cadastro')
+def cadastro():
+    return render_template('modulos/clientes/create.html')
+
+@app.route('/clientes/cadastro/guardar', methods=['POST'])
+def guardar():
+    #capturando dados recebidos do formulário
+    nome = request.form['nome']
+    telefone = request.form['telefone']
+    data = request.form['data']
+
+    #inserindo dados no banco de dados
+    sql = "INSERT INTO clientes(nome,telefone,data) VALUE(%s,%s,%s )"
+    dados = (nome,telefone,data)
+    conexao = mysql.connection
+    cursor = conexao.cursor()
+    cursor.execute(sql,dados)
+    conexao.commit()
+    return redirect('/clientes')
 
 
 

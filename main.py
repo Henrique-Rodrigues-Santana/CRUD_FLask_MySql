43f1252c0b94ec79b0d653ea334325b86d838959
Henrique Rodrigues Santana <henrique.rocdrigue@gmail.com>
2024-09-04 21:47:28 -0300

    Recuperando Dados
    Recuperando dados do db e exibindo no front-end

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# estanciando e configurando mysql
+
+mysql = MySQL()
+
+# dados da conexão 
+
+Host = 'localhost'
+Port = 3306
+User = 'root'
+Pass = ''
+Database = 'db-crud'
+
+app.config['MYSQL_HOST'] = Host
+app.config['MYSQL_PORT'] = Port
+app.config['MYSQL_USER'] = User
+app.config['MYSQL_PASSWORD'] = Pass
+app.config['MYSQL_DB'] = Database
+
+mysql.init_app(app)
+
 
 
 @app.route('/')
@@ -9,7 +31,19 @@ def index():
 
 @app.route('/clientes')
 def index_clientes():
-    return render_template('modulos/clientes/index.html')
+
+    # Criando consulta p1
+    sql = 'SELECT * FROM clientes'
+
+    conexao = mysql.connection
+    cursor = conexao.cursor()
+    cursor.execute(sql)
+    clientes=cursor.fetchall()
+    conexao.commit()
+
+    return render_template('modulos/clientes/index.html', clientes_t = clientes)
+
+
 
 
 
